31064125c219b0ced25869ff703cced21104d10b
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-03-04 22:27:41 -0600

    final touches to table creation code

--- populate_tables.sql
+++
@@ -1,5 +0,0 @@
-\copy Cutlery (id, name, quantity) FROM 'Cutlery.csv' DELIMITER ',' CSV HEADER;
-\copy MenuItemCutlery (menu_item_id, cutlery_id, quantity) FROM 'MenuItemCutlery.csv' DELIMITER ',' CSV HEADER;
-\copy MenuItem (id, name, price, category) FROM 'MenuItem.csv' DELIMITER ',' CSV HEADER;
-\copy OrderedMenuItem (order_id, menuitem_id, quantity) FROM 'OrderedMenuItem.csv' DELIMITER ',' CSV HEADER;
-\copy Order (id, date, time, total_price) FROM 'Order.csv' DELIMITER ',' CSV HEADER;
