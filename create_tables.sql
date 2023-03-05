d16b3738958ae56eb33982ffd5c58e3c2a4377d6
mateoruiz5171 <mateoruiz5171@gmail.com>
2023-03-04 21:07:54 -0600

    quick ID update

--- create_tables.sql
+++ create_tables.sql
@@ -1,36 +1,34 @@
--- library for our UUIDs
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 -- Main Entities
 CREATE TABLE Cutlery (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
+  id VARCHAR(50) PRIMARY KEY UNIQUE,
   name VARCHAR(50),
   quantity INTEGER
 );
 
-CREATE TABLE MenuItemCutlery (
-  menu_item_id UUID REFERENCES MenuItem(id),
-  cutlery_id UUID REFERENCES Cutlery(id),
-  quantity INTEGER
-);
-
 CREATE TABLE MenuItem (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
+  id VARCHAR(36) PRIMARY KEY UNIQUE,
   name VARCHAR(50),
   price FLOAT,
   category VARCHAR(50)
 );
 
--- Relationship Entities
-CREATE TABLE OrderedMenuItem (
-  order_id UUID REFERENCES Order(id),
-  menuitem_id UUID REFERENCES MenuItem(id),
-  quantity INTEGER
-);
-
 CREATE TABLE Order (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
+  id VARCHAR(36) PRIMARY KEY UNIQUE,
   date VARCHAR(50),
   time VARCHAR(50),
   total_price FLOAT
 );
+
+-- Relationship Entities
+CREATE TABLE OrderedMenuItem (
+  order_id VARCHAR(50) REFERENCES Order(id),
+  menuitem_id VARCHAR(50) REFERENCES MenuItem(id),
+  quantity INTEGER
+);
+
+CREATE TABLE MenuItemCutlery (
+  menu_item_id VARCHAR(36) REFERENCES MenuItem(id),
+  cutlery_id VARCHAR(36) REFERENCES Cutlery(id),
+  quantity INTEGER
+);
+
