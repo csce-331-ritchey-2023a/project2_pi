fffb5681d5a5a45c863f57d3fdf89f87d9ae9e92
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-03-06 09:08:08 -0600

    models

---
+++ src/Models/MenuItem.java
@@ -0,0 +1,11 @@
+package src.Models;
+
+import java.util.List;
+
+public class MenuItem {
+    public int id;
+    public String name;
+    public float price;
+    public String category;
+    public List<Cutlery> MenuItemCutlery;
+}
