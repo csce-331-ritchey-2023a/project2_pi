9255a8da73cf0602a9835ccb0597a243351548aa
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-03-10 20:56:39 -0600

    fixed documentation

--- src/Dao/Inventory.java
+++ src/Dao/Inventory.java
@@ -3,9 +3,20 @@ package src.Dao;
 import java.sql.ResultSet;
 
 import src.IDbClient.DbClient;
+import src.Models.Cutlery;
+import src.Models.MenuItem;
 
 public class Inventory{
     private DbClient dbClient;
+    private MenuItemDao menuItemDao;
+    private CutleryDao cutleryDao;
+
+    public Inventory() 
+    {
+        dbClient = new DbClient();
+        menuItemDao = new MenuItemDao();
+        cutleryDao = new CutleryDao();
+    }
     
     public ResultSet get() {
         ResultSet rs = dbClient.executeQuery(
@@ -20,8 +31,14 @@ public class Inventory{
     }
 
     public void add(Object entity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        if (entity instanceof MenuItem) 
+        {
+            menuItemDao.add((MenuItem) entity);
+        }
+        else if (entity instanceof Cutlery)
+        {
+            cutleryDao.add((Cutlery) entity);
+        }
     }
 
     public void update(Object entity) {
