858036c3175655b9cdd9aecab8bf17eab7cecae7
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-03-20 12:11:50 -0500

    removing unecessary bad security practice

--- src/GUI.java
+++ src/GUI.java
@@ -16,28 +16,7 @@ public class GUI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Connection conn = null;
-        try {
-
-          Class.forName("org.postgresql.Driver");
-          conn = DriverManager.getConnection("jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_pi",
-          "csce315331_pi_master", "3.1415");
-        } catch (Exception e) {
-          e.printStackTrace();
-          System.err.println(e.getClass().getName()+": "+e.getMessage());
-          System.exit(0);
-        }
-        JOptionPane.showMessageDialog(null,"Opened database successfully");
-
-        //closing the connection
-        try {
-          conn.close();
-          JOptionPane.showMessageDialog(null,"Test Connection Closed.");
-        } catch(Exception e) {
-          JOptionPane.showMessageDialog(null,"Test Connection NOT Closed.");
-        }
-        
+         
         MainMenu menuFrame = new MainMenu();
         menuFrame.setVisible(true);
     }
