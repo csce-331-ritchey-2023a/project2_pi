b36fc34b7c187d367dca08cd059bd13a322adecf
mateoruiz5171 <mateoruiz5171@gmail.com>
2023-03-20 20:29:22 -0500

    sql

--- select_pop_date.sql
+++
@@ -1,6 +0,0 @@
-SELECT DATE_TRUNC('day', date_time) AS sales_day, SUM(total_price) AS total_sales
-FROM orders
-WHERE date_time >= NOW() - INTERVAL '7 days'
-GROUP BY sales_day
-ORDER BY total_sales DESC
-LIMIT 1;
