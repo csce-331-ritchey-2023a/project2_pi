492137f6d5c6968f77947685a98a4dff76397601
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-03-22 16:17:48 -0500

    getting started on order test (get test)

--- src/test/daoTests/orderDaoTest.java
+++ src/test/daoTests/orderDaoTest.java
@@ -1,6 +1,13 @@
 package test.daoTests;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.Optional;
+
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import Dao.OrdersDao;
@@ -9,18 +16,30 @@ import Models.Order;
 public class orderDaoTest {
     private OrdersDao ordersDao;
     
-    @BeforeEach
+    @BeforeAll
     public void setup() {
         ordersDao = new OrdersDao();
     }
 
     @Test 
     public void testGet() {
+        Optional<Order> optionalOrder = ordersDao.get("fa9a85ba-bb07-11ed-9982-00155deb6281");
         
+        // make sure it exists 
+        assertTrue(optionalOrder.isPresent());
+
+        Order order = optionalOrder.get();
+
+        // check price of order
+        assertEquals(order.total_price, 8.34f);
+
+        // make sure order is not empty
+        assertFalse(order.OrderedMenuItems.isEmpty());
     }
 
     @Test
     public void testGetAll() {
+        
     }
 
 
